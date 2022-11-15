cb4c6a8cfded02adb471630508cc94d9b5fe35fe
Aya EL-Kilany <ayaelkilany735@gmail.com>
2022-11-15 11:43:20 +0200

    edit

--- payment/urls.py
+++ payment/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AllPayments , PaymentById , CreateTransaction
+from .views import AllPayments , PaymentById , CreateTransaction , callback
 
 urlpatterns =[
     path('' , AllPayments),
     path('<int:id>', PaymentById),
-    path('transaction/<int:id>', CreateTransaction)
+    path('transaction/<int:id>', CreateTransaction),
+    path('callback/<int:id>',callback)
 ] 
